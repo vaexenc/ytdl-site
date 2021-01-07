50035004e601158e305ade540f89196cc088d58e
vaexenc <53697802+vaexenc@users.noreply.github.com>
2021-01-07 01:22:12 +0100

    implement query

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 "use strict";
 
+const {exec} = require("child_process");
 const express = require("express");
 const favicon = require("express-favicon");
 const path = require("path");
@@ -16,9 +17,25 @@ const app = express();
 app.use(favicon(path.join(__dirname, "assets/public/favicon.png")));
 app.use("/public", express.static("assets/public"));
 
-// app.post("/request", (req, res) => {
-
-// });
+app.get("/query", (req, res) => {
+	function sendEmptyResponse() {
+		res.json("{}");
+	}
+	const query = req.query;
+	if (!query || !query.q) {
+		sendEmptyResponse();
+		return;
+	}
+	exec("youtube-dl --dump-json " + query.q, (error, stdout, stderr) => {
+		if (error) {
+			sendEmptyResponse();
+		}
+		else {
+			res.set("Content-Type", "application/json");
+			res.end(stdout);
+		}
+	});
+});
 
 app.get("/", (req, res) => {
 	res.sendFile(__dirname + "/assets/index.html", (err) => onError(err));
